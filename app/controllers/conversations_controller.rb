d10c522c2781d844269e77b83bceaa057a794012
Sam Purcell <email": null>
2015-12-02 17:06:04 -0500

    Add start a conversation feature

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,12 +3,29 @@ class ConversationsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @conversations = @topic.conversations.order(updated_at: :desc)
   end
-  
+
   def show
     @conversation = Conversation.find(params[:id])
   end
 
   def recent
     @conversations = Conversation.by_recency
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @conversation = Conversation.new
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @conversation = @topic.conversations.new(name: params[:conversation][:name])
+    @conversation.user_id = current_user.id
+
+    if @conversation.save
+      redirect_to topic_conversations_path(@topic)
+    else
+      render :'conversations/new'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resource :sessions, only: [:create]
   resource :users, only: [:create]
   resources :topics, only: [:index, :show] do
-    resources :conversations, only: [:index, :show]
+    resources :conversations, only: [:index, :show, :new, :create]
   end
   resources :messages, only: [:create]
 
